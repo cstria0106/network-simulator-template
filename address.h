fa38fc1cc29fb4042db1dea41b3ec7365400a710
Lee Sangyeop <cstria0106@gmail.com>
2024-05-28 13:37:06 +0900

    Set address value to string

--- address.h
+++ address.h
@@ -6,11 +6,12 @@
 // 주소를 나타내는 클래스
 class Address {
 private:
-  int address_;
+  std::string address_;
 
 public:
-  Address(int address) : address_(address) {}
-  std::string toString() const { return std::to_string(address_); }
+  Address(const std::string &address) : address_(address) {}
+  Address(int address) : address_(std::to_string(address)) {}
+  std::string toString() const { return address_; }
   bool operator==(const Address &rhs) const { return address_ == rhs.address_; }
 };
 
