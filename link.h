eb3a5b2b91c5e8a498b4f6a119b64a023d637a8f
Lee Sangyeop <cstria0106@gmail.com>
2024-05-28 13:59:40 +0900

    Add delay to link

--- link.h
+++ link.h
@@ -11,11 +11,20 @@ class Link : Object {
   friend class LinkInstaller;
 
 private:
-  Link(Node *nodeA, Node *nodeB) : nodeA_(nodeA), nodeB_(nodeB) {}
+  Link(Node *nodeA, Node *nodeB, double delay = 0.0)
+      : nodeA_(nodeA), nodeB_(nodeB), delay_(delay) {}
 
   Node *nodeA_;
   Node *nodeB_;
-  
+  double delay_;
+
+public:
+  double delay() { return delay_; }
+
+  Node *nodeA() { return nodeA_; }
+
+  Node *nodeB() { return nodeB_; }
+
   // 매개변수로 주어진 노드가 아닌 반대편 노드를 구한다.
   Node *other(const Node *node) const {
     return node == nodeA_ ? nodeB_ : nodeA_;
--- link_installer.h
+++ link_installer.h
@@ -7,7 +7,7 @@
 class LinkInstaller {
 public:
   // 노드 a, b 사이에 링크를 설치하고 반환한다.
-  Link *install(Node *a, Node *b);
+  Link *install(Node *a, Node *b, double delay = 0.0);
 };
 
 #endif
