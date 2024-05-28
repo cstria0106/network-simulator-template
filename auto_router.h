98b1c7bc3233421a62a7777bc0dba605d79083b9
Lee Sangyeop <cstria0106@gmail.com>
2024-05-28 14:00:24 +0900

    Add AutoRouter

---
+++ auto_router.h
@@ -0,0 +1,25 @@
+#ifndef AUTO_ROUTER_H
+#define AUTO_ROUTER_H
+
+#include "host.h"
+#include "link.h"
+#include "router.h"
+#include "service.h"
+#include <algorithm>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+
+class AutoRouter : public Router {
+public:
+  void calculate(const std::vector<Node *> &nodes,
+                 const std::vector<Link *> &links) {
+    // 전체 노드와 링크 정보를 통해
+    // 모든 호스트로 전달될 수 있는 라우팅 테이블을 구성한다
+    // TODO: 구현
+  }
+};
+
+#endif
